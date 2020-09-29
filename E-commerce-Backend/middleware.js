a0099072136d2e69b9959c6d7e2f21213f7f9167
SushantBagri <sushant19@navgurukul.org>
2020-09-29 16:24:49 +0530

    routes

---
+++ E-commerce-Backend/middleware.js
@@ -0,0 +1,11 @@
+const isLoggedIn=(req,res,next)=>{
+    if(req.user){
+        console.log(req.user)
+        next()
+    }
+    else{
+        res.sendStatus(401);
+    }
+}
+
+module.exports=(isLoggedIn)
